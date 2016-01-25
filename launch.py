1cdbeb3ba51253814bac5af72ac26e10ce614968
Aaron DeVera <adevera@fordham.edu>
2016-01-24 22:14:22 -0500

    launch changed

--- launch.py
+++ launch.py
@@ -1,15 +1,22 @@
 import schedule
 import time
+import os
 
 def job():
-    print("I'm working...")
+    os.system('mkdir dailybeehive')
+    os.system('cd dailybeehive')
+    os.system('git clone https://github.com/aaronsdevera/dailybeehive.git')
+    os.system('cd dailybeehive/prod')
+    os.system('python dailybeehive.py -gpst')
+    os.system('cd; rm -rf dailybeehive')
+    
 
-schedule.every(10).minutes.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
-schedule.every().monday.do(job)
-schedule.every().wednesday.at("13:15").do(job)
+schedule.every(.05).minutes.do(job)
+#schedule.every().hour.do(job)
+#schedule.every().day.at("10:30").do(job)
+#schedule.every().monday.do(job)
+#schedule.every().wednesday.at("13:15").do(job)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(10)
